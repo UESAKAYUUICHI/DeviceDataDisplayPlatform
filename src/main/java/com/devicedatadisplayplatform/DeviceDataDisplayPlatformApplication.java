68caf0dfb82680aedc3f9dde3556047e42c0887f
UESAKAYUUICHI <2359739518@qq.com>
2025-12-10 09:43:09 +0800

    开发分支初始化

---
+++ src/main/java/com/devicedatadisplayplatform/DeviceDataDisplayPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.devicedatadisplayplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceDataDisplayPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceDataDisplayPlatformApplication.class, args);
+    }
+
+}
